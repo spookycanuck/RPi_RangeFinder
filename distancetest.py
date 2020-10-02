d2d322c6659891b362a98148d3caa27eed34112d
E <emann@mail.niagara.edu>
2020-10-02 13:00:31 -0400

    create function & persistent while loop

--- distancetest.py
+++ distancetest.py
@@ -7,59 +7,40 @@ GPIO.setwarnings(False)
 TRIG = 17
 ECHO = 18
 LED = 4
+BLUE = 21
 
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 GPIO.setup(LED,GPIO.OUT)
+GPIO.setup(BLUE,GPIO.OUT)
 
-#GPIO.output(TRIG, False)
-#time.sleep(0.1)
-GPIO.output(LED, GPIO.LOW)
-GPIO.output(TRIG, True)
-time.sleep(1)
-GPIO.output(TRIG, False)
+def get_distance():
+    GPIO.output(TRIG, True)
+    time.sleep(0.5)
+    GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO) == False:
-    start = time.time()
-    #print ("GPIO input = ",start)
-    #GPIO.output(LED, GPIO.HIGH)
+    while GPIO.input(ECHO) == False:
+        start = time.time()
 
-while GPIO.input(ECHO) == True:
-    end = time.time()
-    #print ('GPIO output = ',end)
-    #GPIO.output(LED, GPIO.HIGH)
+    while GPIO.input(ECHO) == True:
+        end = time.time()
 
-sig_time = end-start
+    sig_time = end-start
 
+    #inches:
+    distance = sig_time / 0.000148
 
-#CM:
-#distance = sig_time / 0.000058
+    print('Distance: {} inches'.format(distance))
 
-#inches:
-distance = sig_time / 0.000148
+    return distance
 
-print('Distance: {} inches'.format(distance))
-
-if distance <= 8:
-    GPIO.output(LED,GPIO.HIGH)
-    time.sleep(3)
-else:
-    GPIO.output(LED,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(LED,GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(LED,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(LED,GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(LED,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(LED,GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(LED,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(LED,GPIO.LOW)
-    time.sleep(1)
-
+while True:
+    distance = get_distance()
+    if distance <= 8:
+        GPIO.output(LED,GPIO.HIGH)
+	GPIO.output(BLUE,GPIO.LOW)
+    else:
+        GPIO.output(BLUE,GPIO.HIGH)
+	GPIO.output(LED,GPIO.LOW)
 
 GPIO.cleanup()
